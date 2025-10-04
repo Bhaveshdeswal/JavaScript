110c977fe4840306d80eb0b345c1860335577e3c
Bhaveshdeswal <106268802+Bhaveshdeswal@users.noreply.github.com>
2025-10-04 22:53:16 +0000

    Lesson01

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const userId= 936343
+let emailId="deshwalbhavesh.1001@gmail.com"
+var accountPass="3122"
+/*
+Prefer not to use var
+because var is independent of block scope or functional scope.
+*/
+accountCity="Najafgarh"
+let accountState;
+//userId= 926232 // NOT ALLOWED
+accountCity="Najafgarh New Delhi"
+
+console.log(userId);
+console.table([userId, emailId, accountPass, accountCity,accountState])
