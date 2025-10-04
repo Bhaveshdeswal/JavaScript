a89795882c9bb3c3b3fc4eafd4aac32988deceed
Bhaveshdeswal <106268802+Bhaveshdeswal@users.noreply.github.com>
2025-10-04 23:22:54 +0000

    Data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name="Bhavesh"      //String
+let age=23              //Number => 2^53 //bigInt
+let isLoggedIn= true    //Boolean Value
+//null =>stand alone value
+//undefined (not defined)
+/* 
+null is like an empty glass and 
+undefined is like currently I don't know what 
+and how much of the liquid is inside the glass
+*/ 
+// symbol
+
+
+// object
+
+console.log(typeof age)       // number
+console.log(typeof null)      //object
+console.log(typeof undefined) //undefined
