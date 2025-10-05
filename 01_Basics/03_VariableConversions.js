879e4058fc32aec4129986a94abce6fd10562118
Bhaveshdeswal <106268802+Bhaveshdeswal@users.noreply.github.com>
2025-10-05 10:35:40 +0000

    Type Casting

---
+++ 01_Basics/03_VariableConversions.js
@@ -0,0 +1,22 @@
+let score=true
+//console.log(typeof(score))
+let valueInNumber=Number(score)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+/*
+"33"=>33
+"33abc"=>NaN
+null=>0
+undefined=>NaN
+true/false=>1/0
+*/
+let isLoggedIn=-96
+let boolisLoggedIn=Boolean(isLoggedIn)
+//console.log(boolisLoggedIn)
+/*
+""=> false
+"kd"=> true
+0/1 => false/true
+96/-96=>true
+*/
