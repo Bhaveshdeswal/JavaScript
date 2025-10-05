f282101e7632f92e43ee79dea8e6568f8f8710f5
Bhaveshdeswal <106268802+Bhaveshdeswal@users.noreply.github.com>
2025-10-05 15:58:38 +0000

    Comparision Operators

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,4 @@
+// console.log(null>=0)   //gives true because ">=" and "<=" converts null into zero before comparision
+// console.log(null==0)   //false
+console.log("2"==2)    //true
+console.log("2"===2)   //false : === is strict check and checks without type casting
