8e84e517e65c7d2a76265efe182af8b9655a1039
Bhavesh <106268802+Bhaveshdeswal@users.noreply.github.com>
2025-10-15 14:49:57 +0000

    Objects

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -1,4 +1,5 @@
 // Object literals
+
 let mySym=Symbol("Key1")
 let mySym2=Symbol("Key2")
 let jsUser={
@@ -30,5 +31,64 @@ jsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this["Full Name"]}`);
 }
 
-console.log(jsUser.greeting());
-console.log(jsUser.greetingTwo());
+// console.log(jsUser.greeting());
+// console.log(jsUser.greetingTwo());
+
+// Singleton Object
+
+let instaUser= new Object();
+instaUser.id="abc135"
+instaUser.name="Truecaller"
+instaUser.isLoggedIn=false
+let moreDetails={
+    email:"dk@gmail.com",
+    mob: "9878543796"
+}
+instaUser.contactDetails=moreDetails
+// console.log(instaUser);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "q@gmail.com"
+    },
+    {
+        id: 2,
+        email: "w@gmail.com"
+    },
+    {
+        id: 3,
+        email: "e@gmail.com"
+    },
+    {
+        id: 4,
+        email: "r@gmail.com"
+    },
+    {
+        id: 5,
+        email: "t@gmail.com"
+    },
+    {
+        id: 6,
+        email: "y@gmail.com"
+    },
+]
+
+// console.log(users[1].email)
+
+// console.log(instaUser);
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));
